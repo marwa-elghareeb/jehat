67854e31edbe8adda8195731931031df1c83f0eb
ManarSa0 <118136638+ManarSa0@users.noreply.github.com>
2022-12-25 14:42:59 +0300

    update ourteam

--- resources/views/site/ourTeam.blade.php
+++ resources/views/site/ourTeam.blade.php
@@ -1,9 +1,5 @@
 @extends('layouts.appPortal')
-
-
 @section('content')
-
-
 <main>
     <!--? slider Area Start-->
     <section class="slider-area slider-area2">
@@ -26,6 +22,78 @@
     <!--? Our Services Start -->
 
 
+<body>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+        <section class="our-services">
+
+<div class="wrapper">
+    <h1>Our Team</h1>
+    <div class="our_team">
+        <div class="team_member">
+          <div class="member_img">
+           <img src=" {{ asset('public/web-assets/img/11.jpg' ) }}" class="img-fluid  img-thumbnail rounded-circle" alt="image1">
+
+            <div class="social_media">
+               <div class="facebook item"><i class="fab fa-facebook-f"></i></div>
+               <div class="twitter item"><i class="fab fa-twitter"></i></div>
+               <div class="instagram item"><i class="fab fa-instagram"></i></div>
+             </div>
+          </div>
+          <h3>Saad Al Mohsen</h3>
+          <span>Chairman of Board of Directors </span>
+          <p>...............</p>
+        </div>
 
 
+        <div class="team_member">
+           <div class="member_img">
+             <img src="{{ asset('public/web-assets/img/12.jpg  ' ) }}" class="img-fluid  img-thumbnail rounded-circle" alt="image1">
+
+             <div class="social_media">
+               <div class="facebook item"><i class="fab fa-facebook-f"></i></div>
+               <div class="twitter item"><i class="fab fa-twitter"></i></div>
+               <div class="instagram item"><i class="fab fa-instagram"></i></div>
+             </div>
+          </div>
+          <h3>Ahmed Basuony</h3>
+          <span>IT Manager</span>
+          <p>........................................</p>
+      </div>
+
+
+        <div class="team_member">
+           <div class="member_img">
+           <img src=" {{ asset('public/web-assets/img/13.jpg ' ) }}" class="img-fluid  img-thumbnail rounded-circle" alt="image1">
+             <div class="social_media">
+               <div class="facebook item"><i class="fab fa-facebook-f"></i></div>
+               <div class="twitter item"><i class="fab fa-twitter"></i></div>
+               <div class="instagram item"><i class="fab fa-instagram"></i></div>
+             </div>
+          </div>
+          <h3>Datuk Hj Abdul Rahim</h3>
+          <span>CEO of Malaysia</span>
+          <p>...........................</p>
+      </div>
+
+
+        <div class="team_member">
+           <div class="member_img">
+           <img src=" {{ asset('public/web-assets/img/14.jpg' ) }}" class="img-fluid  img-thumbnail rounded-circle" alt="image1">
+             <div class="social_media">
+               <div class="facebook item"><i class="fab fa-facebook-f"></i></div>
+               <div class="twitter item"><i class="fab fa-twitter"></i></div>
+               <div class="instagram item"><i class="fab fa-instagram"></i></div>
+             </div>
+          </div>
+          <h3>Dr . Ayman Mahmoud Helmy</h3>
+          <span>CEO of Egypt</span>
+          <p>........................</p>
+      </div>  
+    </div>
+</div>
+</div>
+
+</body>
+</section>
+</main>
     @endsection
