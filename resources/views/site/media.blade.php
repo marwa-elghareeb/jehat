d6a4fa6e97de725d80caae65835cd07e7e76d5d0
Mousa0099 <kkssaamm0099@gmail.com>
2022-12-25 12:22:02 +0300

    add img2

--- resources/views/site/media.blade.php
+++ resources/views/site/media.blade.php
@@ -90,66 +90,66 @@
 	</div>
 
 	<div class="section-top-border">
-		<h3>شرح منصة مستفيد</h3>
+		<h3>شرح منصة الموظف</h3>
 		<div class="row gallery-item">
 			<div class="col-md-3">
 				<a class="popup-video btn-icon" href="https://www.jehat.sa/media/mp4/Add_dep.mp4" class="video-wrap position-relative">
-					<div class="single-gallery-image" style="background: url(assets/img/elements/g1.jpg);"></div>
-					<h5> انشاء جهه جديده </h5>
+				<img class="single-gallery-image" src="{{ asset('public/web-assets/img/h1.jpg' ) }}">
+					<h5> اضافه وادارة الفواتير الالكترونية</h5>
 				</a>
 			</div>
 			<div class="col-md-3">
 				<a class="popup-video btn-icon" href="https://www.jehat.sa/media/mp4/client_register.mp4" class="video-wrap position-relative">
-					<div class="single-gallery-image" style="background: url(assets/img/elements/g5.jpg);"></div>
-					<h5> الية التسجيل كمستفيد </h5>
+				<img class="single-gallery-image" src="{{ asset('public/web-assets/img/h2.jpg' ) }}">
+					<h5> اضافه ومتابعه الطلبات </h5>
 				</a>
 			</div>
 			<div class="col-md-3">
 				<a class="popup-video btn-icon" href="https://www.jehat.sa/media/mp4/lang.mp4" class="video-wrap position-relative">
-					<div class="single-gallery-image" style="background: url(assets/img/elements/g3.jpg);"></div>
-					<h5> تغيير لغة العرض </h5>
+				<img class="single-gallery-image" src="{{ asset('public/web-assets/img/h3.jpg' ) }}">
+					<h5> ادارة الاجتماع اثناء الانعقاد</h5>
 				</a>
 			</div>
 			<div class="col-md-3">
 				<a class="popup-video btn-icon" href="https://www.jehat.sa/media/mp4/View_Meeting.mp4" class="video-wrap position-relative">
-					<div class="single-gallery-image" style="background: url(assets/img/elements/g8.jpg);"></div>
-					<h5> حضور الاجتماعات والتصويت </h5>
+				<img class="single-gallery-image" src="{{ asset('public/web-assets/img/h4.jpg' ) }}">
+					<h5>التعليقات وانواع الاسناد</h5>
 				</a>
 			</div>
 			<div class="col-md-3">
 				<a class="popup-video btn-icon" href="https://www.jehat.sa/media/mp4/Client_Forget_Password.mp4" class="video-wrap position-relative">
-					<div class="single-gallery-image" style="background: url(assets/img/elements/g4.jpg);"></div>
-					<h5> استرجاع كلمه المرور </h5>
+				<img class="single-gallery-image" src="{{ asset('public/web-assets/img/h5.jpg' ) }}">
+					<h5>ادارة العملاء والاقسام </h5>
 				</a>
 			</div>
 			<div class="col-md-3">
 				<a class="popup-video btn-icon" href="https://www.jehat.sa/media/mp4/Edit_Client.mp4" class="video-wrap position-relative">
-					<div class="single-gallery-image" style="background: url(assets/img/elements/g2.jpg);"></div>
-					<h5> تعديل البيانات الشخصيه </h5>
+				<img class="single-gallery-image" src="{{ asset('public/web-assets/img/h6.jpg' ) }}">
+					<h5> ادارة غرف الفيديو كونفرس</h5>
 				</a>
 			</div>
 			<div class="col-md-3">
 				<a class="popup-video btn-icon" href="https://www.jehat.sa/media/mp4/add_m.mp4" class="video-wrap position-relative">
-					<div class="single-gallery-image" style="background: url(assets/img/elements/g9.jpg);"></div>
-					<h5> اضافه المفوضين واداره المستندات </h5>
+				<img class="single-gallery-image" src="{{ asset('public/web-assets/img/h7.jpg' ) }}">
+					<h5> الية انشاء  الاجتماعات</h5>
 				</a>
 			</div>
 			<div class="col-md-3">
 				<a class="popup-video btn-icon" href="https://www.jehat.sa/media/mp4/add_t.mp4" class="video-wrap position-relative">
-					<div class="single-gallery-image" style="background: url(assets/img/elements/g6.jpg);"></div>
-					<h5> اضافه واداره المواعيد </h5>
+				<img class="single-gallery-image" src="{{ asset('public/web-assets/img/h8.jpg' ) }}">
+					<h5> استخدام فلتر بحث المعاملات</h5>
 				</a>
 			</div>
 			<div class="col-md-3">
 				<a class="popup-video btn-icon" href="https://www.jehat.sa/media/mp4/Add_Transaction.mp4" class="video-wrap position-relative">
-					<div class="single-gallery-image" style="background: url(assets/img/elements/g10.jpg);"></div>
-					<h5> اضافه ومتابعه طلب </h5>
+				<img class="single-gallery-image" src="{{ asset('public/web-assets/img/h9.jpg' ) }}">
+					<h5> ادارة الموظفين </h5>
 				</a>
 			</div>
 			<div class="col-md-3">
 				<a class="popup-video btn-icon" href="https://www.jehat.sa/media/mp4/convert.mp4" class="video-wrap position-relative">
-					<div class="single-gallery-image" style="background: url(assets/img/elements/g7.jpg);"></div>
-					<h5> الانتقال الى مكتبي </h5>
+				<img class="single-gallery-image" src="{{ asset('public/web-assets/img/10.jpg' ) }}">
+					<h5> اضافه واداره المواعيد</h5>
 				</a>
 			</div>
 		</div>
