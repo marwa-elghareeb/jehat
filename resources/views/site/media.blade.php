33bde25cf4cc374f5b2f3412a802ca4324f2945e
Mousa0099 <kkssaamm0099@gmail.com>
2022-12-25 11:13:59 +0300

    add img

--- resources/views/site/media.blade.php
+++ resources/views/site/media.blade.php
@@ -27,62 +27,62 @@
 		<div class="row gallery-item">
 			<div class="col-md-3">
 				<a class="popup-video btn-icon" href="https://www.jehat.sa/media/mp4/Add_dep.mp4" class="video-wrap position-relative">
-					<img class="single-gallery-image" src="{{ asset('public/web-assets/img/g5.jpg' ) }}">
-				
+					<img class="single-gallery-image" src="{{ asset('public/web-assets/img/g1.jpg' ) }}">
+
 					<h5> انشاء جهه جديده </h5>
 				</a>
 			</div>
 			<div class="col-md-3">
 				<a class="popup-video btn-icon" href="https://www.jehat.sa/media/mp4/client_register.mp4" class="video-wrap position-relative">
-					<div class="single-gallery-image" style="background: url(assets/img/elements/g5.jpg);"></div>
+				<img class="single-gallery-image" src="{{ asset('public/web-assets/img/g5.jpg' ) }}">
 					<h5> الية التسجيل كمستفيد </h5>
 				</a>
 			</div>
 			<div class="col-md-3">
 				<a class="popup-video btn-icon" href="https://www.jehat.sa/media/mp4/lang.mp4" class="video-wrap position-relative">
-					<div class="single-gallery-image" style="background: url(assets/img/elements/g3.jpg);"></div>
+				<img class="single-gallery-image" src="{{ asset('public/web-assets/img/g3.jpg' ) }}">
 					<h5> تغيير لغة العرض </h5>
 				</a>
 			</div>
 			<div class="col-md-3">
 				<a class="popup-video btn-icon" href="https://www.jehat.sa/media/mp4/View_Meeting.mp4" class="video-wrap position-relative">
-					<div class="single-gallery-image" style="background: url(assets/img/elements/g8.jpg);"></div>
+				<img class="single-gallery-image" src="{{ asset('public/web-assets/img/g8.jpg' ) }}">
 					<h5> حضور الاجتماعات والتصويت </h5>
 				</a>
 			</div>
 			<div class="col-md-3">
 				<a class="popup-video btn-icon" href="https://www.jehat.sa/media/mp4/Client_Forget_Password.mp4" class="video-wrap position-relative">
-					<div class="single-gallery-image" style="background: url(assets/img/elements/g4.jpg);"></div>
+				<img class="single-gallery-image" src="{{ asset('public/web-assets/img/g4.jpg' ) }}">
 					<h5> استرجاع كلمه المرور </h5>
 				</a>
 			</div>
 			<div class="col-md-3">
 				<a class="popup-video btn-icon" href="https://www.jehat.sa/media/mp4/Edit_Client.mp4" class="video-wrap position-relative">
-					<div class="single-gallery-image" style="background: url(assets/img/elements/g2.jpg);"></div>
+				<img class="single-gallery-image" src="{{ asset('public/web-assets/img/g2.jpg' ) }}">
 					<h5> تعديل البيانات الشخصيه </h5>
 				</a>
 			</div>
 			<div class="col-md-3">
 				<a class="popup-video btn-icon" href="https://www.jehat.sa/media/mp4/add_m.mp4" class="video-wrap position-relative">
-					<div class="single-gallery-image" style="background: url(assets/img/elements/g9.jpg);"></div>
+				<img class="single-gallery-image" src="{{ asset('public/web-assets/img/g9.jpg' ) }}">
 					<h5> اضافه المفوضين واداره المستندات </h5>
 				</a>
 			</div>
 			<div class="col-md-3">
 				<a class="popup-video btn-icon" href="https://www.jehat.sa/media/mp4/add_t.mp4" class="video-wrap position-relative">
-					<div class="single-gallery-image" style="background: url(assets/img/elements/g6.jpg);"></div>
+				<img class="single-gallery-image" src="{{ asset('public/web-assets/img/g6.jpg' ) }}">
 					<h5> اضافه واداره المواعيد </h5>
 				</a>
 			</div>
 			<div class="col-md-3">
 				<a class="popup-video btn-icon" href="https://www.jehat.sa/media/mp4/Add_Transaction.mp4" class="video-wrap position-relative">
-					<div class="single-gallery-image" style="background: url(assets/img/elements/g10.jpg);"></div>
+				<img class="single-gallery-image" src="{{ asset('public/web-assets/img/g10.jpg' ) }}">
 					<h5> اضافه ومتابعه طلب </h5>
 				</a>
 			</div>
 			<div class="col-md-3">
 				<a class="popup-video btn-icon" href="https://www.jehat.sa/media/mp4/convert.mp4" class="video-wrap position-relative">
-					<div class="single-gallery-image" style="background: url(assets/img/elements/g7.jpg);"></div>
+				<img class="single-gallery-image" src="{{ asset('public/web-assets/img/g7.jpg' ) }}">
 					<h5> الانتقال الى مكتبي </h5>
 				</a>
 			</div>
