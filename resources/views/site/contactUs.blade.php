451f51dc7e5be180985b6639cd1c31e8ca6ef10c
Mousa0099 <kkssaamm0099@gmail.com>
2022-12-26 15:18:26 +0300

    .

--- resources/views/site/contactUs.blade.php
+++ resources/views/site/contactUs.blade.php
@@ -39,9 +39,82 @@
             </div>
         </div>
     </section>
+    
     <!--?  Contact Area start  -->
     <section class="contact-section">
         <div class="container">
+        <div class="row">
+                <div class="col-12">
+                    <h2 class="contact-title">ابق على اتصال</h2>
+                </div>
+                <div class="col-lg-8">
+                    <form class="form-contact contact_form" 
+                    action="contact_process.php" method="post" id="contactForm"
+                        novalidate="novalidate">
+                        <div class="row">
+                     
+                       
+                       <div class="col-sm-6">
+                           <div class="form-group">
+                               <input class="form-control valid" name="name" id="name" type="text"
+                                   onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter your name'"
+                                   placeholder="الأسم">
+                           </div>
+                       </div>
+                       <div class="col-6">
+                           <div class="form-group">
+                               <input class="form-control" name="subject" id="subject" type="text"
+                                   onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Subject'"
+                                   placeholder="رقم الهاتف">
+                           </div>
+                       </div>
+                       <div class="col-sm-12">
+                           <div class="form-group">
+                               <input class="form-control valid" name="email" id="email" type="email"
+                                   onfocus="this.placeholder = ''"
+                                   onblur="this.placeholder = 'Enter email address'"
+                                   placeholder="البريد الالكتروني">
+                           </div>
+                       </div>
+                     
+                       <div class="col-12">
+                           <div class="form-group">
+                               <textarea class="form-control w-100" name="message" id="message" cols="30" rows="9"
+                                   onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Message'"
+                                   placeholder=" محتوى الرسالة"></textarea>
+                           </div>
+                       </div>
+                   </div>
+                       
+                        <div class="form-group mt-3">
+                            <button type="submit" class="button button-contactForm boxed-btn">ارسال</button>
+                        </div>
+                    </form>
+                </div>
+                <div class="col-lg-3 offset-lg-1">
+                    <div class="media contact-info">
+                        <span class="contact-info__icon"><i class="ti-home"></i></span>
+                        <div class="media-body">
+                            <h3>السعودية</h3>
+                            <p>الرياض, مخرج 24</p>
+                        </div>
+                    </div>
+                    <div class="media contact-info">
+                        <span class="contact-info__icon"><i class="ti-tablet"></i></span>
+                        <div class="media-body">
+                            <h3>+966553777769</h3>
+                            <p>من الاحد الى الخميس من الساعة ٩ صباحا الى ٥ مساء</p>
+                        </div>
+                    </div>
+                    <div class="media contact-info">
+                        <span class="contact-info__icon"><i class="ti-email"></i></span>
+                        <div class="media-body">
+                            <h3> info@jehat.sa</h3>
+                            <p>على مدار 24 ساعة</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div class="d-none d-sm-block mb-5 pb-4">
                 <div id="map" style="height: 480px; position: relative; overflow: hidden;">
                     <div
@@ -493,78 +566,7 @@
                     </script>
 
             </div>
-            <div class="row">
-                <div class="col-12">
-                    <h2 class="contact-title">ابق على اتصال</h2>
-                </div>
-                <div class="col-lg-8">
-                    <form class="form-contact contact_form" 
-                    action="contact_process.php" method="post" id="contactForm"
-                        novalidate="novalidate">
-                        <div class="row">
-                     
-                       
-                       <div class="col-sm-6">
-                           <div class="form-group">
-                               <input class="form-control valid" name="name" id="name" type="text"
-                                   onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter your name'"
-                                   placeholder="الأسم">
-                           </div>
-                       </div>
-                       <div class="col-6">
-                           <div class="form-group">
-                               <input class="form-control" name="subject" id="subject" type="text"
-                                   onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Subject'"
-                                   placeholder="رقم الهاتف">
-                           </div>
-                       </div>
-                       <div class="col-sm-12">
-                           <div class="form-group">
-                               <input class="form-control valid" name="email" id="email" type="email"
-                                   onfocus="this.placeholder = ''"
-                                   onblur="this.placeholder = 'Enter email address'"
-                                   placeholder="البريد الالكتروني">
-                           </div>
-                       </div>
-                     
-                       <div class="col-12">
-                           <div class="form-group">
-                               <textarea class="form-control w-100" name="message" id="message" cols="30" rows="9"
-                                   onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Message'"
-                                   placeholder=" محتوى الرسالة"></textarea>
-                           </div>
-                       </div>
-                   </div>
-                       
-                        <div class="form-group mt-3">
-                            <button type="submit" class="button button-contactForm boxed-btn">ارسال</button>
-                        </div>
-                    </form>
-                </div>
-                <div class="col-lg-3 offset-lg-1">
-                    <div class="media contact-info">
-                        <span class="contact-info__icon"><i class="ti-home"></i></span>
-                        <div class="media-body">
-                            <h3>السعودية</h3>
-                            <p>الرياض, مخرج 24</p>
-                        </div>
-                    </div>
-                    <div class="media contact-info">
-                        <span class="contact-info__icon"><i class="ti-tablet"></i></span>
-                        <div class="media-body">
-                            <h3>+966553777769</h3>
-                            <p>من الاحد الى الخميس من الساعة ٩ صباحا الى ٥ مساء</p>
-                        </div>
-                    </div>
-                    <div class="media contact-info">
-                        <span class="contact-info__icon"><i class="ti-email"></i></span>
-                        <div class="media-body">
-                            <h3> info@jehat.sa</h3>
-                            <p>على مدار 24 ساعة</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            
         </div>
     </section>
   
