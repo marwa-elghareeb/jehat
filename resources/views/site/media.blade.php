d0da4654f0a80f4adb16d3dae316ae9b3a25be19
ManarSa0 <118136638+ManarSa0@users.noreply.github.com>
2023-01-01 20:37:24 +0300

    Update media.blade.php

--- resources/views/site/media.blade.php
+++ resources/views/site/media.blade.php
@@ -36,13 +36,13 @@
 border-radius:1rem
 }
 .card {
-    text-align: center;
+    text-align: right;
     display: flex;
     flex-direction: column;
     align-items: center;
-    padding-top: 30px;
+    padding-top: 0px;
     padding-right: 25px;
-    padding-bottom: 50px;
+    padding-bottom: 0px;
     padding-left: 0px
 }
 .container3 {
@@ -52,14 +52,14 @@ border-radius:1rem
     margin-bottom: 0px;
     margin-left: auto;
     padding-top: 0px;
-    padding-right: 30px;
+    padding-right: 0px;
     padding-bottom: 0px;
-    padding-left: 30px;
+    padding-left: 900px;
 }
 
 .section-title h2 {
   font-family: 'Cairo';
-  font-size: 20px;
+  font-size: 18px;
   text-transform: uppercase;
   position: relative;
   color: #444347;
@@ -76,17 +76,28 @@ border-radius:1rem
 		<div class="slider-active">
 			<!-- Single Slider -->
 			<div class="single-slider slider-height2">
-				<div class="container">
-					<div class="row justify-content-center">
+		</div>
+		</div>
+	</section>
+
+	<section>
+
+	<div class="container3">
+	<div class="row justify-content-center">
 						<div class="col-xl-8 col-lg-11 col-md-12">
 							<div class="hero__caption hero__caption2 text-right">
-								<h2 data-animation="bounceIn" data-delay="0.2s"> 
+								<h1 data-animation="bounceIn" data-delay="0.2s"> 
 								
 							المركز الاعلامي 
 
 							
      
-            <div class="card col-12 col-md-12 col-lg-3">
+           </h1>
+						
+					</div>
+				</div>
+			</div>
+<div class="card col-12 col-md-12 col-lg-3">
 			<div class="section-title h2"style="text-align:right">
 
 	<a href="{{ asset('public/web-assets/presentation.pdf' ) }}"  " >
@@ -98,15 +109,15 @@ border-radius:1rem
 	<br>
 	
 	
-      
-            <div class="card col-12 col-md-12 col-lg-3">
+     
+          <div class="card col-12 col-md-12 col-lg-3">
 			<div class="section-title h2" style="text-align:right">
 	<a href="{{ asset('public/web-assets/user guide.pdf' ) }}"  "> 
 	
 	<h2> تحميل ملف دليل المستخدم  <img src=" {{ asset('public/web-assets/img/icon/pdficon.svg' ) }} " alt="" style="height: 40px; width: 40px;"> </h2></a>
 	</div>      
 </div>
-
+</div>
 	
 </h2>
 
@@ -114,45 +125,13 @@ border-radius:1rem
 							</div>
 							
 						</div>
-						
-					</div>
-				</div>
-			</div>
-		</div>
-		
-	</section>
 
 
 
-
-
-
-
-
-
-
-
-          
-
-
-
-
-
-               
-      
-	
-
-			
-  
-
-
-
-   
-
-
-
-
-
+</section>
+<br>
+<br>
+<br>
 
 
 	<div class="section-top-border">
