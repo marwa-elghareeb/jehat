17939122c4bc737bb06633a84986384f225aa286
marwa-elghareeb <marwa.job.mans@gmail.com>
2023-01-15 14:47:09 +0200

    update design

--- resources/views/site/footerPages.blade.php
+++ resources/views/site/footerPages.blade.php
@@ -1,101 +1,41 @@
 @extends('layouts.appPortal')
-
 @section('content')
 
-<style> 
-
-.slider-area.slider-area2,
-.slider-area2.slider-area2 {
-    background-image: url('public/web-assets/img/hero/jehatp90.png')
-}
-
-
-.card{
-border-radius:4rem
-}
-
-.container1 {
-    max-width: 1380px;
-    margin-top: -320px;
-    margin-right: auto;
-    margin-bottom: 0px;
-    margin-left: auto;
-    padding-top: 0px;
-    padding-right: 30px;
-    padding-bottom: 0px;
-    padding-left: 30px;
-}
-h1{
-     font-family: 'Cairo';
-    color: #000;
-	font-size: 30px;
-    margin-top: 0px;
-    font-style: normal;
-    font-weight: 400;}
-p {
-     font-family: 'Cairo';
-    color: #000;
-	font-size: 15px;
-    margin-top: 0px;
-    font-style: normal;
-    font-weight: 400;}
-
-</style>
-
-  <section class="slider-area slider-area2">
-        <div class="slider-active">
-            <!-- Single Slider -->
-            <div class="single-slider slider-height2">
-                <div class="container">
-                    <div class="row justify-content-center">
-                        <div class="col-xl-8 col-lg-11 col-md-12">
-                           
-                        </div>
-                    </div>
-                </div>
+<!-------------------------------------------------------------------------------->
+<div class="breadcrumb">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h2 class="breadcrumbs-title"> صفحات <span> الفوتر</span></h2>
+                <ul class="breadcrumb-list">
+                    <li><a href="#">الرئيسية</a></li>
+                    <li>صفحات الفوتر  </li>
+                </ul>
             </div>
         </div>
-    </section>
-
-
-
-  <div class="inner-page pb-5 ">
-	<div class="inner-page-container">
-        <div class="container1">
+    </div>
+</div>
+<!-------------------------------------------------------------------------------->
+<div class="layout-pages">
+    <div class="container">
         <div class="row justify-content-center">
-            <div class="card col-9 col-md-10 col-lg-15">
-                <div class="card-wrapper">
-                    <div class="card-box align-center">
-                      <br>
-<section class="sample-text-area">
-			<div class="container box_1170" style="text-align : right">
-				
-				<p class="sample-text">
-					
-<h1>شروط استخدام المنصة </h1>
-<br>
+            <div class=" col-xxl-12 col-lg-12 col-md-12 col-12">
+                <div class="card">
+                    <div class="card-wrapper">
+                       
 
-<p><b> إن دخولك واستخدامك للموقع يخضع للشروط والأحكام الموضحة أدناه، بالإضافة إلى قوانين المملكة العربية السعودية، دخولك إلى الموقع يعني موافقتك على هذه الشروط والأحكام، سواء كنت مستخدماً مسجلاً أم لا. وبدخولك وتصفحك واستخدامك للموقع فإنك تكون قد قبلت، من دون قيد أو شرط، أحكام وشروط الاستخدام الواردة في هذه الوثيقة.
+                    
+                    </div>
 
-أنت توافق على دخول واستخدام الموقع لأغراض مشروعة فقط، وأنت مسؤول مسؤولية كاملة عن العلم والعمل بأي من وكل القوانين والأنظمة والقواعد والأحكام المتعلقة باستخدامك للموقع. وبمجرد دخولك إلى الموقع فإنك توافق على الامتناع عن: <br> <br></b>
+                </div>
+            </div>
 
+        </div>
+    </div>
 
-
-استخدام الموقع من أجل ارتكاب جرم أو تشجيع الآخرين على التورط في أي تصرف قد يعد جريمة معلوماتية.<br> <br>
-إدخال أو نشر أي محتويات غير قانونية تتضمن تمييزاً أو تشهيراً أو إساءة.<br> <br>
-استخدام الموقع من أجل انتحال شخصيات أو أطراف أخرى.<br> <br>
-استخدام الموقع لتحميل أي مادة فيها برامج تحتوي على فيروسات، أو أي شفرات حاسوبية، أو ملفات، أو برامج قد تعمل على تغيير، أو إتلاف أو إعاقة عمل الموقع أو أي جهاز أو برنامج عائد إلى أي شخص يدخل إلى الموقع.<br> <br>
-تغيير أو إتلاف أو شطب أي محتوى على الموقع.<br> <br>
-الادعاء بالارتباط مع، أو تمثيل أي فرد، شركة أو مؤسسه حكومية أو خاصه من دون أن تكون مخولاً بادعاء تلك العلاقة أو ذلك التمثيل.<br> 
-نشر، أو بث أي إعلان، أو مادة دعائية، أو أي شكل من أشكال الترويج.<br> <br>
-نشر أي مادة تتنافى أو تتعارض مع حقوق الملكية الفكرية للآخرين.<br><br> 
-جمع أو تخزين المعلومات الشخصية عن الآخرين<br><br> </p>
-					
-				</p>
-			</div></div></div></div></div></div></div></div>
-		</section>
-<main>
+</div>
 
 
-</main>
+
+
 @endsection
