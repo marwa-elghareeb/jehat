8c503929c16499f24ddf7bd20dafff853edcf506
Nasser rashed Alquraini <Nasar12341244@gmail.com>
2023-01-17 14:50:05 +0300

    show in media controller

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Midia extends Model
+class Media extends Model
 {
     use HasFactory;
 }
---
+++ resources/views/admin/media/list.blade.php
@@ -0,0 +1,133 @@
+@extends('layouts.appAdmin')
+@section('content')
+
+
+<!--**********************************
+            Content body start
+        ***********************************-->
+<div class="content-body">
+    <div class="container-fluid">
+        <div class="row page-titles">
+            <div class="col p-md-0">
+                <h4>Media</h4>
+            </div>
+            <div class="col p-md-0">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="{{ route('media-data.index') }}">Cards</a>
+                    </li>
+                    <li class="breadcrumb-item active">Show Media</li>
+                </ol>
+            </div>
+        </div>
+
+
+
+
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="media button-Media">
+                    <div class="media-body">
+                        <a href="{{ route('media-data.create') }}" 
+                        type="button" class="btn btn-rounded btn-info" style="float: right;">
+                            <span class="btn-icon-left text-info">
+                                <i class="fa fa-plus color-info"></i> </span>Add</a>
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="content-wrapper">
+            @if(Session::has('flash_message'))
+            <p class="alert alert-info">{{ Session::get('flash_message') }}</p>
+            @endif
+        </div>
+
+        <div class="row">
+            <div class="col-12">
+                <div class="media">
+                    <div class="media-body">
+                        <div class="table-responsive">
+                            <table class="example display" style="min-width: 845px">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Name (Ar)</th>
+                                        <th>Name (En)</th>
+                                
+                                        <th>Edit</th>
+                                        <th>Delete</th>
+
+                                    </tr>
+                                </thead>
+                                <tbody>
+
+                                    <?php $i = 1; ?>
+                                    @foreach($allData as $data)
+                                    <tr>
+                                        <th>{{ $i ++ }}</th>
+                                        <td>{{ $data->name_ar }}</td>
+                                        <td>{{ $data->name_en }}</td>
+
+                                    
+
+                                        <td>
+                                            <a href="{{ route('media-data.edit', $data->id ) }}" type="button" class="btn btn-rounded btn-secondary">
+                                                <i class="fa fa-pencil color-secondary"></i> </a>
+                                        </td>
+
+                                        <td>
+
+                                            <form method="POST" action="{{route('media-data.destroy', $data->id) }}">
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-rounded btn-danger">
+                                                    <i class="fa fa-close color-danger"></i></button>
+                                                @csrf
+                                            </form>
+
+
+
+                                        </td>
+
+                                    </tr>
+
+
+                                    @endforeach
+
+                                <tfoot>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Name (Ar)</th>
+                                        <th>Name (En)</th>
+                                       
+                                        <th>Edit</th>
+                                        <th>Delete</th>
+
+                                    </tr>
+                                </tfoot>
+
+
+                                </tbody>
+
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+
+        </div>
+    </div>
+    <!-- #/ container -->
+</div>
+<!--**********************************
+            Content body end
+        ***********************************-->
+
+
+@endsection
