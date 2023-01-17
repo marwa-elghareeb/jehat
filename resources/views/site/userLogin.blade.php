a4a91f9488edeb0a2904fd4299d370d787e3101f
marwa-elghareeb <marwa.job.mans@gmail.com>
2023-01-17 14:41:08 +0200

    user login design

--- resources/views/site/userLogin.blade.php
+++ resources/views/site/userLogin.blade.php
@@ -9,7 +9,7 @@
                 <h2 class="breadcrumbs-title"> تسجيل <span> الدخول</span></h2>
                 <ul class="breadcrumb-list">
                     <li><a href="{{ URL :: to ('/')}}">الرئيسية</a></li>
-                    <li>  تسجيل الدخول </li>
+                    <li> تسجيل الدخول </li>
                 </ul>
             </div>
         </div>
@@ -18,26 +18,78 @@
 
 
 <section id="services" class="services jehat-stage">
-        <div class="container">
-            <div class="card">
-                <div class="row">
-                    <div class="col-12">
-                        <div class="section-title" data-aos="fade-up">
-                            <h1 data-animation="bounceIn" data-delay="0.2s"> تسجيل الدخول</h1>
+    <div class="container">
+        <div class="card">
+            <div class="row">
+                <div class="col-12">
+                    <div class="section-title" data-aos="fade-up">
+                        <h1 data-animation="bounceIn" data-delay="0.2s"> تسجيل الدخول</h1>
 
-                        </div>
                     </div>
                 </div>
-              
-
-
-
-
-
             </div>
-        </div>
 
-    </section>
+
+
+
+            <form>
+                <!-- Email input -->
+                <div class="form-outline mb-4">
+                    <input type="email" id="form2Example1" class="form-control" />
+                    <label class="form-label" for="form2Example1">Email address</label>
+                </div>
+
+                <!-- Password input -->
+                <div class="form-outline mb-4">
+                    <input type="password" id="form2Example2" class="form-control" />
+                    <label class="form-label" for="form2Example2">Password</label>
+                </div>
+
+                <!-- 2 column grid layout for inline styling -->
+                <div class="row mb-4">
+                    <div class="col d-flex justify-content-center">
+                        <!-- Checkbox -->
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="form2Example31" checked />
+                            <label class="form-check-label" for="form2Example31"> Remember me </label>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <!-- Simple link -->
+                        <a href="#!">Forgot password?</a>
+                    </div>
+                </div>
+
+                <!-- Submit button -->
+                <button type="button" class="btn btn-primary btn-block mb-4">Sign in</button>
+
+                <!-- Register buttons -->
+                <div class="text-center">
+                    <p>Not a member? <a href="#!">Register</a></p>
+                    <p>or sign up with:</p>
+                    <button type="button" class="btn btn-link btn-floating mx-1">
+                        <i class="fab fa-facebook-f"></i>
+                    </button>
+
+                    <button type="button" class="btn btn-link btn-floating mx-1">
+                        <i class="fab fa-google"></i>
+                    </button>
+
+                    <button type="button" class="btn btn-link btn-floating mx-1">
+                        <i class="fab fa-twitter"></i>
+                    </button>
+
+                    <button type="button" class="btn btn-link btn-floating mx-1">
+                        <i class="fab fa-github"></i>
+                    </button>
+                </div>
+            </form>
+
+        </div>
+    </div>
+
+</section>
 
 
 @endsection
