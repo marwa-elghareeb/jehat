b229a66443e81b9c4275caaaa6b87d45f1cb4b4e
Mousa0099 <kkssaamm0099@gmail.com>
2023-01-17 18:10:42 +0300

    login

--- resources/views/site/userLogin.blade.php
+++ resources/views/site/userLogin.blade.php
@@ -33,29 +33,28 @@
                 <form>
   <!-- Email input -->
   <div class="form-outline mb-4">
+  <label class="form-label" for="form2Example1">Email address</label>
     <input type="email" id="form2Example1" class="form-control" />
-    <label class="form-label" for="form2Example1">Email address</label>
+    
   </div>
 
   <!-- Password input -->
   <div class="form-outline mb-4">
+  <label class="form-label" for="form2Example2">Password</label>
     <input type="password" id="form2Example2" class="form-control" />
-    <label class="form-label" for="form2Example2">Password</label>
+    
   </div>
 
   <!-- 2 column grid layout for inline styling -->
   <div class="row mb-4">
     <div class="col d-flex justify-content-center">
       <!-- Checkbox -->
-      <div class="form-check">
-        <input class="form-check-input" type="checkbox" value="" id="form2Example31" checked />
-        <label class="form-check-label" for="form2Example31"> Remember me </label>
-      </div>
+     
     </div>
 
     <div class="col">
       <!-- Simple link -->
-      <a href="#!">Forgot password?</a>
+     
     </div>
   </div>
 
@@ -64,8 +63,7 @@
 
   <!-- Register buttons -->
   <div class="text-center">
-    <p>Not a member? <a href="#!">Register</a></p>
-    <p>or sign up with:</p>
+    
     <button type="button" class="btn btn-link btn-floating mx-1">
       <i class="fab fa-facebook-f"></i>
     </button>
