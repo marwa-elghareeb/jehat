0a688b3511a3afe27400dcf371a5997d1304fa66
Nasser rashed Alquraini <Nasar12341244@gmail.com>
2023-01-22 15:21:35 +0300

    Update

--- app/Http/Controllers/Site/MediaSiteController.php
+++ app/Http/Controllers/Site/MediaSiteController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers\Site;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Media;
 
 class MediaSiteController extends Controller
 {
     public function index()
     {
         return view('site.media');
+        $allData = Media::all();
+        return view('site.media')->with(['allData' => $allData]); 
     }
 }
