0ab7cc58388ac48c5299dcff89aba1f611136ef5
bahiyyah_4 <bahiab144@gmail.com>
2023-02-15 11:58:26 +0300

    update add role

--- database/migrations/2023_02_15_070417_create_role_table.php
+++ database/migrations/2023_02_15_070417_create_role_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateRoleTable extends Migration
+class CreateRolesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateRoleTable extends Migration
      */
     public function up()
     {
-        Schema::create('role', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->string('name_ar');
             $table->string('name_en');
---
+++ resources/views/admin/role/create.blade.php
@@ -0,0 +1,86 @@
+@extends('layouts.appAdmin')
+@section('content')
+
+
+<div class="content-body">
+    <div class="container-fluid">
+        <div class="row page-titles">
+            <div class="col p-md-0">
+                <h4>role</h4>
+            </div>
+            <div class="col p-md-0">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="{{ route('role.index') }}">role</a>
+                    </li>
+
+                    <li class="breadcrumb-item active">Add 
+                    </li>
+                </ol>
+            </div>
+        </div>
+
+        <div class="row">
+
+            <div class="col-xl-12">
+
+
+                <div class="card forms-card">
+                    <div class="card-body">
+                        <h4 class="card-title mb-4">Add New </h4>
+                        <div class="basic-form">
+                            <form role="form" enctype="multipart/form-data" method="post" action="{{ route('role.store') }}">
+                                {{ csrf_field() }}
+
+
+                                <div class="form-group row align-items-center">
+                                    <label class="col-sm-3 col-form-label text-label"> Name (Ar)</label>
+                                    <div class="col-sm-9">
+                                        <div class="input-group">
+                                            <input type="text" class="form-control" name="name_ar" id="validationDefaultUsername1" aria-describedby="validationDefaultUsername1">
+                                        </div>
+                                        @if($errors->has('name_ar'))
+                                        <div class="error">{{ $errors->first('name_ar') }}</div>
+                                        @endif
+                                    </div>
+                                </div>
+
+                                <div class="form-group row align-items-center">
+                                    <label class="col-sm-3 col-form-label text-label"> Name (En)</label>
+                                    <div class="col-sm-9">
+                                        <div class="input-group">
+                                            <input type="text" class="form-control" name="name_en" id="validationDefaultUsername1" aria-describedby="validationDefaultUsername1">
+                                        </div>
+                                        @if($errors->has('name_en'))
+                                        <div class="error">{{ $errors->first('name_en') }}</div>
+                                        @endif
+                                    </div>
+                                </div>
+
+                               
+                         
+                               
+                                
+
+
+                                <div style="float: right;">
+                                    <button type="submit" class="btn btn-primary btn-form">Submit</button>
+                                    <button type="reset"  class="btn btn-light btn-form"><a href="{{ route('role.index') }}">Cancel</a></button>
+
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+
+
+        </div>
+    </div>
+    <!-- #/ container -->
+</div>
+
+@endsection
